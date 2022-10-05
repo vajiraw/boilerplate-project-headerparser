68e81795cccadd29be673be2e7bf669e5e4e202b
Vajira <vajirawke@gmail.com>
2022-10-05 20:20:45 +0530

    implement get user ip,software,language

--- index.js
+++ index.js
@@ -24,6 +24,14 @@ app.get('/api/hello', function (req, res) {
   res.json({ greeting: 'hello API' });
 });
 
+app.get('/api/whoami',(req,res)=>{
+  res.json({
+    'ipaddree':req.ip,
+    'language':req.acceptsLanguages(),
+    'software': req.get('User-Agent')
+  });
+})
+
 // listen for requests :)
 var listener = app.listen(process.env.PORT || 3000, function () {
   console.log('Your app is listening on port ' + listener.address().port);
